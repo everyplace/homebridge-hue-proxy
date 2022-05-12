52964b0a96d11efe1dbc255792efc96c953829d8
root <root@OpenWrt.lan>
2022-05-12 13:12:45 +0000

    Added --host param

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-docker build . -t everyplace/homebridge-hue-proxy-alpine;
+#!/bin/sh
+docker build . -t everyplace/homebridge-hue-proxy-alpine --network host;
 docker save everyplace/homebridge-hue-proxy-alpine > hue-proxy.tar;
